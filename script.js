d1e2738d9e783e965a0e1789048ad430331c1fc3
rachel4victoria <rachel4victoria@gmail.com>
2024-05-31 23:18:10 -0300

    Atualizações

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const iniciarOuPausarIcon = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
 
-let tempoDecorridoEmSegundos = 5
+let tempoDecorridoEmSegundos = 1500
 let intervaloId = null
 
 musica.loop = true
@@ -28,22 +29,26 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
+   tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+   tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+   tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
-    botoes.forEach(function (contexto){
+      mostrarTempo()
+      botoes.forEach(function (contexto){
         contexto.classList.remove('active')
     })
     html.setAttribute('data-contexto', contexto)
@@ -77,7 +82,7 @@ const contagemRegressiva = () => {
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+      mostrarTempo()
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
@@ -100,3 +105,11 @@ function zerar() {
     iniciarOuPausarIcon.setAttribute ('src' , `/imagens/play_arrow.png`)
     intervaloId = null
 }
+
+function mostrarTempo(){
+   const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+   const tempoFormatado =  tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+   tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
